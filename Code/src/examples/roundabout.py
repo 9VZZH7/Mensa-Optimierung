e41c2102bf645a35091143cb0c3e749c4262d7f5
Jannik Hausladen <jannik.hausladen@fau.de>
2023-01-14 13:08:01 +0100

    removed new import

--- Code/src/examples/roundabout.py
+++ Code/src/examples/roundabout.py
@@ -1,10 +1,4 @@
 import numpy as np
-
-from inspect import getsourcefile
-import os.path as path, sys
-current_dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
-sys.path.insert(0, current_dir[:current_dir.rfind(path.sep)])
-
 from trafficSimulator import *
 
 sim = Simulation()
