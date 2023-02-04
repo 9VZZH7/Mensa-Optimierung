9465f3b7bbe4c5a55d601e8f2fcf572778fbefe2
Jannik Hausladen <jannik.hausladen@fau.de>
2023-02-04 12:52:47 +0100

    [ADD] Then He said, "Let there be parallelity," and there was was parallelity

--- Code/src/tests/mensa_test.py
+++ Code/src/tests/mensa_test.py
@@ -6,7 +6,8 @@ from trafficSimulator import *
 from examples import mensa
 from scipy import interpolate as ip 
 
-import multiprocessing as mp
+from functools import partial
+from joblib import Parallel, delayed
 
 def test_east_west():
     eva = []
@@ -41,7 +42,7 @@ def var_speed_and_dist(N_speed, N_dist):
             eva[i,j] = sim.norm
     x, y = np.meshgrid(dists, speeds)
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(x, y, eva, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax.plot_surface(x, y, eva, cmap=cm.RdYlGn_r, linewidth=0, antialiased=False)
     plt.show()
     return eva
 
@@ -49,15 +50,19 @@ def par_speed_and_dist(N_speed, N_dist):
     eva = np.zeros((N_speed,N_dist + 1))
     speeds = np.arange(10, 20, 10/N_speed)
     dists = np.arange(0,1 + 1/N_dist, 1/N_dist)
-    pool = mp.Pool(mp.cpu_count())
-    for i, dist in enumerate(dists):
-            eva[:, i] = [pool.apply(mensa.run,args = ('whole', speed, 'variable', 'variable', spawning(dist, 0.5, 0.5), False, 10)) for speed in speeds]
+    for i in range(len(speeds)):
+        speed = speeds[i]
+        helper = partial(mensa_helper,steps = 'whole', fixed_cycle = False, v_max = speed)
+        output = Parallel(n_jobs=6)(delayed(helper)(dist) for dist in dists)
+        eva[i,:] = output
     x, y = np.meshgrid(dists, speeds)
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(x, y, eva, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax.plot_surface(x, y, eva, cmap=cm.RdYlGn_r, linewidth=0, antialiased=False)
     plt.show()
-    pool.close()
     return eva
+
+def mensa_helper(dist,steps, fixed_cycle, v_max):
+    return mensa.run(weights = spawning(dist, 0.5, 0.5), steps = steps, fixed_cycle = fixed_cycle, v_max = v_max).norm
 
 def test_diff_spawning():
     all_same = (2,2,1,1,1,1,2,2)
@@ -70,3 +75,22 @@ def test_diff_spawning():
     mid_heavy_sim = mensa.run(steps = 'whole', fixed_cycle = False, v_weight = 'const', weights = const_spawning(*mid_heavy), v_rate = 20)
     
     return all_same_sim, side_heavy_sim, mid_heavy_sim
+
+def plot_diff_spawning():
+    ass, shs, mhs = test_diff_spawning()
+    #fig = plt.figure(figsize = (5,5))
+    #ax = fig.add_axes([0,0,1,1])
+    fig, ax = plt.subplots()
+    dists = ['Jedes Essen gleich', 'Essen an der Seite beliebt', 'Essen in der Mitte beliebt']
+    norms = [ass.norm, shs.norm, mhs.norm]
+    ax.bar(dists,norms)
+    ax.set_ylabel('Waiting Norm')
+    ax.set_title('Influence of distribution on waiting times')
+    plt.show()
+    
+def test_tabletts_abholen():
+    times = []
+    lim = [2, 4, 8, 10, float('inf')]
+    for i in lim:
+        times.append(mensa.run(steps = 'whole', fixed_cycle = False, speed_lim = i).norm)
+    plt.plot(lim,times)
