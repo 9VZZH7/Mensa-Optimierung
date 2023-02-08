c57a34696083b8ee76d8b4009aada4fa431e971b
Jannik Hausladen <jannik.hausladen@fau.de>
2023-02-08 11:10:39 +0100

    [MOD] examples now functional

--- Code/src/tests/beispiel_2.py
+++ Code/src/tests/beispiel_2.py
@@ -2,3 +2,4 @@ from trafficSimulator import *
 from examples import mensa, mensa_with_checkouts
 
 mensa.run(steps = 200, v_weight = 'variable', weights = spawning(0.5,0.5,0.5), fixed_cycle = False)
+pygame.quit()
